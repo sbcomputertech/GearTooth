cdd37aaec7b0f983d339249d061327c962ba1591
reddust9 <78689018+sbcomputertech@users.noreply.github.com>
2022-10-10 15:51:06 +0100

    Update run_gtil.py

--- gtre/run_gtil.py
+++ gtre/run_gtil.py
@@ -6,7 +6,7 @@ il = None
 with open(filename, "rb") as f:
     zl = zlib.decompress(f.read())
     b = base64.b64decode(zl)
-    il = b.decode().replace("\n" * 5, "\n")
+    il = b.decode().replace("@o\"''''", "0")
 
 code = compile(il, filename, "exec")
 exec(code)
